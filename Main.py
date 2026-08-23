dda435246023a924c9be981a614407de2dce6a98
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-23 15:14:10 +0530

    add get item by id

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from router import router as items_router
-
+from fastapi import HTTPException
 app = FastAPI(title="Item Store API", version="1.0.0")
 
 app.include_router(items_router)
@@ -12,3 +12,15 @@ def read_root():
 @router.get("/", response_model=list[ItemResponse])
 def get_items():
     return load_items()
+@router.get("/{item_id}", response_model=ItemResponse)
+def get_item(item_id: int):
+    items = load_items()
+
+    for item in items:
+        if item["id"] == item_id:
+            return item
+
+    raise HTTPException(
+        status_code=404,
+        detail="Item not found"
+    )
