acd431b9b3769509e25c3d917797a3d3ccbc298d
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-23 15:08:44 +0530

    add create item end point

--- router.py
+++ router.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from schemas import ItemCreate, ItemResponse
+from storage import load_items, save_items
+from datetime import datetime
 
 router = APIRouter(prefix="/items", tags=["Items"])
 
@@ -6,3 +9,20 @@ router = APIRouter(prefix="/items", tags=["Items"])
 @router.get("/health")
 def items_health():
     return {"status": "items router active"}
+@router.post("/", response_model=ItemResponse)
+def create_item(item: ItemCreate):
+    items = load_items()
+
+    new_item = {
+        "id": len(items) + 1,
+        "name": item.name,
+        "price": item.price,
+        "description": item.description,
+        "offer": item.offer,
+        "created_at": datetime.now().isoformat()
+    }
+
+    items.append(new_item)
+    save_items(items)
+
+    return new_item
