1b72ab2efb1c3f3f50495c1ced7e525567ab653a
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-23 14:49:56 +0530

    updated main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,11 @@
-from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
-app=FastAPI()
-class Item(BaseModel):
-    name: str
-    price: float
-    offer: Union[float, None] = None
+from routes import router as items_router
+
+app = FastAPI(title="Item Store API", version="1.0.0")
+
+app.include_router(items_router)
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id} 
